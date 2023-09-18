325e006eb30891d0be423d104064d47af0901003
GeorgeRNG <grogrng@gmail.com>
2023-09-18 15:19:47 +0100

    I started this code from chapter 3 tbh

---
+++ src/syntax.ts
@@ -0,0 +1,11 @@
+export function tokeniser (input: string): Token[] {
+    const out: Token[] = []
+    let currentPosition = 0
+  
+    while (currentPosition < input.length) {
+      // Process token, increment currentPosition
+    }
+  
+    return out
+}
+  
