a23d6b1c13bf4a2f5ea72974bcd1a077b73fdec7
GeorgeRNG <grogrng@gmail.com>
2023-09-19 15:18:41 +0100

    progress

--- src/index.ts
+++ src/index.ts
@@ -11,21 +11,34 @@ const Game = new TokenWord('scope-game',/G(AME)?/i);
 const Save = new TokenWord('scope-save',/S(AVE)?/i);
 const GlobalScope = new TokenOption('global-scope',[Game,Save]);
 
+const LeftCurlyBracket = new TokenWord('bracket-curly-left','{');
+const RightCurlyBracket = new TokenWord('bracket-curly-right','}');
+const LeftSquareBracket = new TokenWord('bracket-square-left','[');
+const RightSquareBracket = new TokenWord('bracket-square-right',']');
 const LeftAngleBracket = new TokenWord('bracket-angle-left','<');
 const RightAngleBracket = new TokenWord('bracket-angle-right','>');
 const Comma = new TokenWord('comma',',');
-const CommaOptional = new TokenWord('comma-optional',/,?/);
 const FordSlash = new TokenWord('slash-forward',/\//);
 const BackSlash = new TokenWord('slash-back',/\\/);
-const Separator = new TokenOption('separator',[Comma,WhiteSpace]);
+const Separator = new TokenWord('separator',/(\s*,\s*)|(\s+)/);
 
-const Number = new TokenWord('number',/-?\d+(.\d+)?/);
+//#region
+const Number = new TokenWord('number',/-?\d+(\.\d+)?/);
 const String = new TokenWord('string',/"[^"]+"/i);
 const Vector = new TokenShape('vector',[LeftAngleBracket,Number,Separator,Number,Separator,Number,RightAngleBracket]);
+
+const LocationFull = new TokenShape('location-full',[Number,Separator,Number,Separator,Number,Separator,Number,Separator,Number]);
+const LocationBlock = new TokenShape('location-block',[Number,Separator,Number,Separator,Number]);
+const LocationData = new TokenOption('location-data',[LocationFull,LocationBlock]);
+const Location = new TokenShape('location',[LeftSquareBracket,LocationData,RightSquareBracket])
+
 const TextOpen = new TokenShape('text-open',[LeftAngleBracket,RightAngleBracket]);
 const TextClose = new TokenShape('text-close',[LeftAngleBracket,FordSlash,RightAngleBracket]);
 const Text = new TokenShape('text',[TextOpen,VariableName,TextClose]);
-const Literal = new TokenOption('value',[Number,String,Vector,Text]);
+
+const Literals = [Number,String,Vector,Text,Location];
+const Literal = new TokenOption('value',Literals);
+//#endregion
 
 const Assignment = new TokenWord('assignment','=');
 const VariableAssignment = new TokenShape('variable-assignment',[WhiteSpaceOptional,Assignment,WhiteSpaceOptional]);
@@ -34,6 +47,12 @@ const VariableAssignmentLiteral = new TokenShape('variable-assignment-literal',[
 const BigVariable = new TokenShape('big-variable',[GraveAccent,VariableName,GraveAccent]);
 const SmallVariable = new TokenWord('small-variable',/[A-Z_][A-Z0-9_]+/i);
 const Variable = new TokenOption('variable',[BigVariable,SmallVariable]);
-const GlobalVariable = new TokenShape('global',[GlobalScope,WhiteSpace,Variable,VariableAssignmentLiteral]);
+const GlobalVariable = new TokenShape('global',[GlobalScope,WhiteSpace,Variable]);
+const GlobalVariableAssignmentLiteral = new TokenShape("global-assign",[GlobalVariable,VariableAssignmentLiteral]);
 
-export const StorageScope = new TokenPool('game',[WhiteSpace,LineEnd,GlobalVariable]);
+const Player = new TokenWord('player',/player/i);
+const Entity = new TokenWord('entity',/ent(ity)/i);
+const EventScopeName = new TokenOption('event-scope-name',[Player,Entity]);
+const EventScope = new TokenPool('event-scope',[EventScopeName,WhiteSpaceOptional,LeftCurlyBracket,RightCurlyBracket]);
+
+export const StorageScope = new TokenPool('game',[WhiteSpace,LineEnd,GlobalVariableAssignmentLiteral,GlobalVariable,EventScope]);
